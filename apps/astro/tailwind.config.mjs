7b9452c8923e6bdf725e17131a5286f0e9a2391c
SurajSSingh <22825536+SurajSSingh@users.noreply.github.com>
2025-01-27 19:45:14 -0800

    chore(astro): Add code for working with Skeleton in Astro
    Known issue: Components do not work yet due to issue

--- apps/astro/tailwind.config.mjs
+++ apps/astro/tailwind.config.mjs
@@ -1,9 +1,15 @@
 import { skeleton } from '@skeletonlabs/skeleton/plugin';
 import * as themes from '@skeletonlabs/skeleton/themes';
+import { contentPath } from '@skeletonlabs/skeleton';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		// FIXME: Giving error: [postcss] (0 , _skeleton.contentPath) is not a function
+		// contentPath(import.meta.url, 'react'),
+		// contentPath(import.meta.url, 'svelte'),
+	],
 	theme: {
 		extend: {},
 	},
